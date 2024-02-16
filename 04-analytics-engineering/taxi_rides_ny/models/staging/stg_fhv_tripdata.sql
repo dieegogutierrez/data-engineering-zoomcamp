e504f9b6647edf71b525db774a2ff414ad5cde2d
Diego Gutierrez <dieego_gutierrez@hotmail.com>
2024-02-16 23:10:08 +0000

    add limit to fhv tripdata

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -28,3 +28,9 @@ select
     cast(drop_off_datetime as timestamp) as dropoff_datetime,
 from tripdata
 
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
